35005429a694e8489d79c63c791b82da994ed5d7
Ayushi Batham <ayushibatham60@gmail.com>
2023-04-22 20:35:48 +0530

    updating ui

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,8 +10,6 @@ import Home from "./components/home";
             <Router>
                 <Routes>
                     <Route exact path="/" element={<Home/>}/>
-                    
-                    
                 </Routes>
             </Router>
         </>
